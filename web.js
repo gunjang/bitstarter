f3cb4c2cf9271822eddd9b941e76d8e54545903e
Gunjan Gupta <gunjanandy@yahoo.com>
2013-07-08 22:30:40 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var fs = require('fs');
+  var message = fs.readFileSync("index.html");
+
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
